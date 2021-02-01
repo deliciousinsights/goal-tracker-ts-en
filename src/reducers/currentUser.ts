f982d25cd34a9f2d54bd351b36d0c20a4d96f695
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:45:07 +0100

    Start step 7: Current User slice basics

---
+++ src/reducers/currentUser.ts
@@ -0,0 +1,14 @@
+import { createReducer } from '@reduxjs/toolkit'
+
+// Action Creators
+// ---------------
+
+export const logIn = 'FIXME'
+export const logOut = 'FIXME'
+
+// Reducer
+// -------
+
+export default createReducer(null, (builder) => {
+  // FIXME
+})
