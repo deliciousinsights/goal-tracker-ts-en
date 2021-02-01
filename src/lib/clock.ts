7ca4d38acb5977b0b11f1a30a9bc5e636b8bfe2c
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 23:18:22 +0100

    Step 19: web notifications on reset

--- src/lib/clock.ts
+++ src/lib/clock.ts
@@ -76,5 +76,21 @@ function notify({
     return
   }
 
-  const notif = '???' // 👈🏻 Your code here instead
+  const requireInteraction =
+    typeof window.Notification.prototype.close === 'function' &&
+    secondsVisible > 0
+  const notif = new window.Notification(title, {
+    body: text,
+    icon,
+    lang: 'en',
+    requireInteraction,
+    tag: 'goal-tracker',
+    vibrate: [100, 50, 100, 50, 100],
+  })
+
+  if (requireInteraction) {
+    notif.addEventListener('show', () => {
+      setTimeout(() => notif.close(), secondsVisible * 1000)
+    })
+  }
 }
