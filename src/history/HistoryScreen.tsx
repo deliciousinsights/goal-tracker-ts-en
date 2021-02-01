4aa0f536a7b7d0fa6b12c4bdb419305fa42c4e05
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 23:05:23 +0100

    Step 15: Document Titles

--- src/history/HistoryScreen.tsx
+++ src/history/HistoryScreen.tsx
@@ -1,3 +1,9 @@
+import { useEffect } from 'react'
+
 export default function HistoryScreen() {
+  useEffect(() => {
+    document.title = 'My history'
+  }, [])
+
   return <h1>History coming soon</h1>
 }
