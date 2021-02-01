90eb192f09cf9c1772b092879a0dfcd71bf45986
Christophe Porteneuve <tdd@tddsworld.com>
2021-02-01 22:24:17 +0100

    Step 1: scaffold HomeScreen and LoginScreen

--- src/auth/LoginScreen.tsx
+++ src/auth/LoginScreen.tsx
@@ -6,6 +6,48 @@ import CardContent from '@mui/material/CardContent'
 import CardHeader from '@mui/material/CardHeader'
 import TextField from '@mui/material/TextField'
 
+import classes from './LoginScreen.module.css'
+import TogglablePasswordField from './TogglablePasswordField'
+
 export default function LoginScreen() {
-  return <p>COMING SOON: LOGIN SCREEN</p>
+  return (
+    <form>
+      <Card className={classes.loginScreen}>
+        <CardHeader title='Goal Tracker' subheader='Sign in' />
+        <CardContent>
+          <TextField
+            autoComplete='home email'
+            fullWidth
+            id='email'
+            label='E-mail'
+            margin='normal'
+            name='email'
+            placeholder='my@email.tld'
+            required
+            type='email'
+          />
+          <TogglablePasswordField
+            autoComplete='current-password'
+            fullWidth
+            id='password'
+            label='Password'
+            margin='normal'
+            name='password'
+            placeholder='one heck of a password'
+            required
+          />
+        </CardContent>
+        <CardActions style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button
+            color='primary'
+            startIcon={<ArrowForward />}
+            type='submit'
+            variant='contained'
+          >
+            Sign in
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
+  )
 }
